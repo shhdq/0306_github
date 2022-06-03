1a61fc8c05a4a5ee6e9e298819d85e608dcbcbcb
VladislavsHH <vladh@latnet.lv>
2022-06-03 19:36:32 +0300

    Darīts!

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ public class Main {
         System.out.println("Hello RCS!");
         System.out.println("Hello Development");
         System.out.println("Madars B");
-        System.out.println("Lasma E");
-        System.out.println("Sandis K");
-        System.out.println("Ricards O");
-        System.out.println("Ardijs B");
-
+        System.out.println(Vladislavs2);
 
     }
 }
